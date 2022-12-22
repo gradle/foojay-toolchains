c92d1f44bb3de326484dbdca0f852e95084b2ab3
József Bartók <jbartok@gradle.com>
2022-12-22 12:12:28 +0200

    Enable build scans on CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,16 @@
+plugins {
+    id("com.gradle.enterprise") version("3.12.1")
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
 
 rootProject.name = "foojay-toolchains"
 include("foojay-resolver")
