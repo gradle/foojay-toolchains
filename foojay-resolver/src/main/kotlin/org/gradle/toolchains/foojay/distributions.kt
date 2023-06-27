bef9d15b2bf8b5c05672f7a62acb42f5d8a73af8
Sebastian Schuberth <sschuberth@gmail.com>
2023-06-27 19:46:06 +0200

    Add remaining distribution properties
    For completeness, list all properties, even if some of them are not used
    (yet).
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- foojay-resolver/src/main/kotlin/org/gradle/toolchains/foojay/distributions.kt
+++ foojay-resolver/src/main/kotlin/org/gradle/toolchains/foojay/distributions.kt
@@ -5,6 +5,7 @@ import org.gradle.jvm.toolchain.JavaLanguageVersion
 import org.gradle.jvm.toolchain.JvmImplementation
 import org.gradle.jvm.toolchain.JvmVendorSpec
 import org.gradle.jvm.toolchain.internal.DefaultJvmVendorSpec.any
+import java.net.URI
 
 @Suppress("DEPRECATION")
 val vendorAliases = mapOf(
@@ -105,6 +106,26 @@ data class Distribution(
      * https://github.com/foojayio/discoapi#endpoint-distributions
      */
     val api_parameter: String,
+
+    /**
+     * A flag to indicate whether the distribution is still maintained or not.
+     */
+    val maintained: Boolean,
+
+    /**
+     * A flag to indicate whether this is an OpenJDK (re-)distribution.
+     */
+    val build_of_openjdk: Boolean,
+
+    /**
+     * A flag to indicate whether this is a GraalVM (re-)distribution.
+     */
+    val build_of_graalvm: Boolean,
+
+    /**
+     * The URI of the offical homepage of the ditribution.
+     */
+    val official_uri: URI,
 
     /**
      * A list of alterative names / spellings for the distribution.
