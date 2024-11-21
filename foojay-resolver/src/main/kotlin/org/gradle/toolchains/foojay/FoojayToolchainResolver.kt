a8cb85b53dd7a676db88a7e611aa311be8fa4ef3
Rhys <98863820+rhysdh540@users.noreply.github.com>
2024-11-21 14:43:01 -0500

    allow using x64 java on arm macs

--- foojay-resolver/src/main/kotlin/org/gradle/toolchains/foojay/FoojayToolchainResolver.kt
+++ foojay-resolver/src/main/kotlin/org/gradle/toolchains/foojay/FoojayToolchainResolver.kt
@@ -3,6 +3,8 @@ package org.gradle.toolchains.foojay
 import org.gradle.jvm.toolchain.JavaToolchainDownload
 import org.gradle.jvm.toolchain.JavaToolchainRequest
 import org.gradle.jvm.toolchain.JavaToolchainResolver
+import org.gradle.platform.Architecture
+import org.gradle.platform.OperatingSystem
 import java.util.*
 
 abstract class FoojayToolchainResolver: JavaToolchainResolver {
@@ -19,7 +21,19 @@ abstract class FoojayToolchainResolver: JavaToolchainResolver {
             platform.operatingSystem,
             platform.architecture
         )?.links
-        val uri = api.toUri(links)
+        var uri = api.toUri(links)
+
+        // Also try x64 on M-series Macs
+        if(uri == null && platform.operatingSystem == OperatingSystem.MAC_OS && platform.architecture == Architecture.AARCH64) {
+            uri = api.toUri(api.toPackage(
+                spec.languageVersion.get(),
+                spec.vendor.get(),
+                spec.implementation.get(),
+                platform.operatingSystem,
+                Architecture.X86_64
+            )?.links)
+        }
+
         return Optional.ofNullable(uri).map(JavaToolchainDownload::fromUri)
     }
 }
