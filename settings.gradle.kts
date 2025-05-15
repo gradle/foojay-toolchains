2cf016567f54d9f0a39d80e5ec2a177e8e6767d1
Louis Jacomet <louis@gradle.com>
2025-05-15 17:47:52 +0200

    Update conventions plugin
    Previous version was causing an issue on CI for publishing jobs.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.gradle.develocity") version "4.0.1"
-    id("io.github.gradle.gradle-enterprise-conventions-plugin").version("0.10.2")
+    id("io.github.gradle.gradle-enterprise-conventions-plugin").version("0.10.3")
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
 }
 
